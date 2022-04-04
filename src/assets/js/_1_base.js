62181eb5a3be94d4484eca051a1427ce1b020481
Nitin Kumar <nitin2953jio@gmail.com>
2022-04-04 19:27:09 +0530

    Added `debounce()` & `tabindex` for `<pre>`

--- src/assets/js/_1_base.js
+++ src/assets/js/_1_base.js
@@ -6,3 +6,35 @@ const body = document.body;
 	if (document.fonts.check("1em Inter")) html.classList.add("fonts-loaded");
 }); */
 
+
+// make a debounce function
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this,
+			args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+}
+
+const pre = document.querySelectorAll("pre");
+
+function log() {
+	pre.forEach(el => {
+		if (el.scrollHeight > el.offsetHeight || el.scrollWidth > el.offsetWidth) {
+			el.setAttribute("tabindex", "0");
+		} else {
+			el.removeAttribute("tabindex");
+		}
+	});
+	console.log("resizing");
+}
+log();
+window.addEventListener("resize", debounce(log, 500));
