f6b734f7fdb676ba6e276667de9f6f240d5e70d8
Nitin Kumar <nitin2953jio@gmail.com>
2022-04-07 18:40:44 +0530

    Simplified function and output text

--- src/assets/i-js/tools/character-counter.js
+++ src/assets/i-js/tools/character-counter.js
@@ -6,41 +6,27 @@
 // })
 
 !(()=>{
-
 	const text = document.getElementById('text');
 	const output1 = document.getElementById('result-1');
 	const output2 = document.getElementById('result-2');
 	const output3 = document.getElementById('result-3');
 	const output4 = document.getElementById('result-4');
 
-	// output text length
-	function textLength() {
-		output1.value = text.value.length;
+	function output() {
+		value = text.value;
+
+		// output text length
+		output1.value = value.length;
+
+		// output text length without more than 1 space between words
+		output2.value = value.replace(/\s+/g, ' ').length;
+
+		// output word count without conuting spaces
+		output3.value = value.split(/\s+/).filter(Boolean).length;
+
+		// paragraph count without empty lines
+		output4.value = value.split(/\n/).filter(line => line.trim()).length;
 	}
 
-	// output text length without more than 1 space between words
-	function textLengthWithoutWhitespace() {
-		output2.value = text.value.replace(/\s+/g, ' ').length;
-	}
-
-	// output word count without conuting spaces
-	function wordCount() {
-		output3.value = text.value.split(/\s+/).filter(Boolean).length;
-	}
-
-	// paragraph count without empty lines
-	function paragraphCountWithoutEmptyLines() {
-		output4.value = text.value.split(/\n/).filter(line => line.trim()).length;
-	}
-
-	function runAll() {
-		console.log("running");
-		textLength();
-		textLengthWithoutWhitespace();
-		wordCount();
-		paragraphCountWithoutEmptyLines();
-	}
-
-	text.addEventListener("input", runAll);
-
+	text.addEventListener("input", output);
 })();
