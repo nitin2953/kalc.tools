48deba92e294e827e9eadf823762f8f1a44a0071
Nitin Kumar <nitin2953jio@gmail.com>
2022-04-24 19:24:59 +0530

    Added sentance count in character counter

--- src/assets/i-js/tools/character-counter.js
+++ src/assets/i-js/tools/character-counter.js
@@ -7,10 +7,11 @@
 
 !(()=>{
 	const text = document.getElementById('text');
-	const output1 = document.getElementById('result-1');
-	const output2 = document.getElementById('result-2');
-	const output3 = document.getElementById('result-3');
-	const output4 = document.getElementById('result-4');
+	const output1 = document.getElementById('1');
+	const output2 = document.getElementById('2');
+	const output3 = document.getElementById('3');
+	const output4 = document.getElementById('4');
+	const output5 = document.getElementById('5');
 
 	function output() {
 		value = text.value;
@@ -26,6 +27,20 @@
 
 		// paragraph count without empty lines
 		output4.value = value.split(/\n/).filter(line => line.trim()).length;
+
+		// sentence count
+		// output5.value = value.split(/[.!?]+./g).filter(Boolean).length;
+		// output5.value = value.match(/[.?!…]+./g).length;
+		// output5.value = value.replace(/\s/g, ' ').match(/[.?!…]+./g).length;
+
+		// match all sentences with a dot at the end
+		// output5.value = value.match(/[.?!…]+./g).length;
+
+		// length of periods with a preceding space and any character
+		// output5.value = value.match(/[.?!…]\s+./g).length;
+
+		// match [.?!…] with a preceding space, new line, or end of string
+		output5.value = value.match(/[.?!…]\s+|\n|$/g).length; // TODO: Fix this
 	}
 
 	text.addEventListener("input", output);
