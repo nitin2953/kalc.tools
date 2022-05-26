073449c68f227fa75f8a2045a62beffee400cda6
Nitin Kumar <nitin2953jio@gmail.com>
2022-05-26 15:19:14 +0530

    Fixed date diff, using `jc-temporal`

--- src/assets/i-js/calculators/time/age.js
+++ src/assets/i-js/calculators/time/age.js
@@ -15,22 +15,28 @@
 
 	const dateFormat = document.getElementById("date-locale");
 	dateFormat.textContent = new Date().toLocaleDateString(); //
-	dateFormat.onclick = ()=>{ aInput.value = "2022-02-28"; bInput.value = "2022-04-01"; calcAge() }; //
+	dateFormat.onclick = () => { aInput.value = "2022-02-28"; bInput.value = "2022-04-01"; calcAge() }; //
 
 	bInput.valueAsDate = new Date();
 
-	function daysInMonth(month, year) {
-		return new Date(year, month, 0).getDate();
-  	}
 
+	function dateDiff(date1, date2) {
+		return Temporal.Calendar.from('iso8601').dateUntil(
+			Temporal.PlainDate.from(date1),
+			Temporal.PlainDate.from(date2),
+			{ largestUnit: 'year' }
+		);
+	}
+
+	function checkBDay(a, b) {
+		return a.day == b.day && a.month == b.month
+	}
 
 	function calcAge() {
 		if (calcForm.checkValidity()) {
 
-			const aDate = aInput.valueAsDate; // DOB
-			const bDate = bInput.valueAsDate; // Today
-
-			if (aDate > bDate) { age.textContent = "NOT BORN YET"; return; }
+			const aDate = aInput.valueAsDate;
+			const bDate = bInput.valueAsDate;
 
 			const a = {
 				year: aDate.getFullYear(),
@@ -43,55 +49,23 @@
 				day: bDate.getDate()
 			}
 
+			let diff = dateDiff(aInput.value, bInput.value);
 
-			let diffYear, diffDay, diffMonth;
-
-			diffYear = b.year - a.year;
-
-			if (a.day <= b.day) diffDay = b.day - a.day;
-			// else if (a.day < b.day) {
-			// 	b.day = b.day + 31;
-			// 	diffDay = b.day - a.day;
-			// 	b.month = b.month - 1;
-			// 	console.log("else if 1");
-			// }
-			// else if (a.day > b.day && a.month < b.month) {
-			// 	b.day = b.day + daysInMonth(a.month, a.year);
-			// 	diffDay = b.day - a.day;
-			// 	b.month = b.month - 1;
-			// 	console.log("else if 2");
-			// }
-			else {
-				// b.day = b.day + 31;
-				b.day = b.day + daysInMonth(a.month, a.year);
-				diffDay = b.day - a.day;
-				b.month = b.month - 1;
-				// diffMonth = diffMonth - 1;
-				// diffMonth = 11;
-				// diffYear = diffYear - 1;
-				console.log("else");
-			}
-
-			if (a.month <= b.month) diffMonth = b.month - a.month;
-			else {
-				b.month = b.month + 12;
-				diffMonth = b.month - a.month;
-				diffYear = diffYear - 1;
-			}
-			console.log(diffYear);
-
-
-			// Birthday
-			if (a.day == b.day && a.month == b.month) {
+			if (checkBDay(a, b)) {
 				bday.style.display = "block";
-				age.textContent = `Age: ${diffYear} years, ${diffMonth} months, ${diffDay} days`;
+				age.textContent = `Age: ${diff.years} years | ${diff.months} months | ${diff.days} days`;
 				return;
 			} else bday.style.display = "none";
 
+			if (aInput.valueAsNumber < bInput.valueAsNumber) {
+				age.textContent = `Age: ${diff.years} years | ${diff.months} months | ${diff.days} days`;
+			} else {
+				diff = dateDiff(bInput.value, aInput.value);
+				age.textContent = `Will born in ${diff.years} years | ${diff.months} months | ${diff.days} days`;
+			}
 
-			age.textContent = `Age: ${diffYear} years, ${diffMonth} months, ${diffDay} days`;
-
-		} else age.textContent = "Age: - years | - months | - days";
+		}
+		else age.textContent = "Age: - years | - months | - days";
 	}
 	calcInputs.forEach(input => input.addEventListener("input", calcAge));
 
